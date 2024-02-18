be96fb813fd33ea60802873ddfc3f8e7176dc5b9
Meriam <mbsalarzon@gmail.com>
2024-02-18 15:33:05 +0000

    Added & modified further CSS styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,17 +22,6 @@ function Contact() {
         <button type="submit">Send Message</button>
       </form>
 
-      <div className="additional-info">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula risus eu mauris
-          lacinia, vel accumsan urna accumsan. Vestibulum tempus enim a eros ultrices, at cursus ex
-          varius. Sed vitae accumsan felis. In hac habitasse platea dictumst.
-        </p>
-        <p>
-          Nullam vel turpis sit amet metus fringilla dignissim in in neque. Duis vitae sapien at
-          risus luctus luctus. Aliquam euismod tortor in pharetra posuere.
-        </p>
-      </div>
     </div>
   );
 }
